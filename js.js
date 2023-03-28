0c123e7a83af26ee6392a73e3f1cf6c70d00bf84
alex6061 <39526012+alex6061@users.noreply.github.com>
2023-03-28 19:26:20 +0300

    commit

--- js.js
+++ js.js
@@ -1,28 +1,36 @@
 //Нет проверки на пустые массивы - они объекты, но ключей у них не будет.
 //Пример работающего кода:
-removeEmpty(obj) {
-    const finalObj = {};
-    Object.keys(obj).forEach((key) => {
-      if (obj[key] && typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
-        const nestedObj = this.removeEmpty(obj[key]);
-        if (Object.keys(nestedObj).length) {
-          finalObj[key] = nestedObj;
-        }
-      } else if (Array.isArray(obj[key])) {
-        if (obj[key].length) {
-          obj[key].forEach((x) => {
-            const nestedObj = this.removeEmpty(x);
-            if (Object.keys(nestedObj).length) {
-              finalObj[key] = finalObj[key] ? [...finalObj[key], nestedObj] : [nestedObj];
-            }
-          });
-        }
-      } else if (obj[key] !== '' && obj[key] !== undefined && obj[key] !== null) {
-        finalObj[key] = obj[key];
-      }
-    });
-    return finalObj;
-  }
+// removeEmpty(obj) {
+//     const finalObj = {};
+//     Object.keys(obj).forEach((key) => {
+//       if (obj[key] && typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+//         const nestedObj = this.removeEmpty(obj[key]);
+//         if (Object.keys(nestedObj).length) {
+//           finalObj[key] = nestedObj;
+//         }
+//       } else if (Array.isArray(obj[key])) {
+//         if (obj[key].length) {
+//           obj[key].forEach((x) => {
+//             const nestedObj = this.removeEmpty(x);
+//             if (Object.keys(nestedObj).length) {
+//               finalObj[key] = finalObj[key] ? [...finalObj[key], nestedObj] : [nestedObj];
+//             }
+//           });
+//         }
+//       } else if (obj[key] !== '' && obj[key] !== undefined && obj[key] !== null) {
+//         finalObj[key] = obj[key];
+//       }
+//     });
+//     return finalObj;
+//   }
 
 //Но в реальных задачах в принципе очень круто познакомится с lodash. Попробуй использовать.
 
+// меня зовут Алексей, я живу в Симферополе
+
+const name = 'Алексей';
+const town = 'Симферополь';
+
+const text = `Меня зовут ${name}, я живу в ${town}`;
+
+console.log (text)
